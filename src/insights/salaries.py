3b0a019f71bac92d3f75b6343a77334588ab3b77
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-12-08 17:03:00 -0300

    feat: Requisito 8 - Função matches_salary_range retornando se o salario está dentro da faixa salarial do job

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -16,30 +16,27 @@ def get_min_salary(path: str) -> int:
     )
 
 
+def check_params(job: Dict, salary: Union[int, str]) -> bool:
+    if "min_salary" not in job or "max_salary" not in job:
+        return False
+
+    if salary == str and not salary.isdigit():
+        return False
+
+    if int(job["min_salary"]) > int(job["max_salary"]):
+        return False
+
+    return True
+
+
+# ajuda na monitoria para solução do requisito
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        if not check_params(job, salary):
+            raise ValueError
+        return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
+    except TypeError:
+        raise ValueError
 
 
 def filter_by_salary_range(
