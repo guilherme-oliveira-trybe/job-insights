9a7446d668834cfa7394042e978c86c3937a010f
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-12-09 16:59:44 -0300

    test: função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in jobs_list:
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job
