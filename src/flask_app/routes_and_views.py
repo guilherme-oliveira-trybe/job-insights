73339f20333cd2ccaeac333527fbbe81aabaa757
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-12-15 12:19:11 -0300

    feat: Requisito 13 - Adicona rota /job/<index> retornando template com as informações especificas de cada job por index informado nos parametos da requisição

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -20,6 +20,7 @@ from src.flask_app.more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -77,6 +78,14 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def jobs_index(index):
+    all_jobs = read(path="data/jobs.csv")
+    job_by_index = get_job(all_jobs, index)
+
+    return render_template("job.jinja2", job=job_by_index)
 
 
 def init_app(app: Flask):
